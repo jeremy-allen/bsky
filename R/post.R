858183ff7ddfbddc2234018e7bde428e8fba7d4f
Jeremy <hijeremyallen@gmail.com>
2024-12-14 09:23:17 -0500

    include reply count in scoring and console printing

--- R/post.R
+++ R/post.R
@@ -7,7 +7,7 @@ source("R/score.R")
 # authenticate to bsky
 auth <- bs_auth(user = bs_get_user(), pass = bs_get_pass())
 
-bs <- function(query = "rstats", n_posts = 2000, keep = FALSE) {
+bs <- function(query = "rstats", n_posts = 1000, keep = FALSE) {
 
   # search for posts
   rstats_posts_raw <- bs_search_posts(
@@ -18,21 +18,18 @@ bs <- function(query = "rstats", n_posts = 2000, keep = FALSE) {
 
   # score degree of sociality of all posts
   posts_scored <- rstats_posts_raw |> 
-    select(uri, like_count, repost_count, author, record) |> 
+    select(uri, like_count, repost_count, reply_count, author, record) |> 
     unnest_wider(c(author, record), names_sep = "_") |>
     distinct() |> 
     mutate (
       date = as_date(with_tz(ymd_hms(record_createdAt, tz = "UTC"),
       tzone = "America/New_York"))
     ) |> 
-    select(uri, author_did, like_count, repost_count, author_displayName,
-      record_text, date, author_handle) |> 
+    select(uri, author_did, like_count, repost_count, reply_count,
+      author_displayName, record_text, date, author_handle) |> 
     mutate(post_url = build_url(did = author_did, uri = uri)) |> 
     # calculate and column for sociality score
     calculate_post_scores()
-
-  all_date_span <- as.integer(difftime(max(posts_scored$date, na.rm = TRUE), 
-    min(posts_scored$date, na.rm = TRUE), units = "days") + 1)
   
   n_keep <- 100
 
@@ -49,21 +46,41 @@ bs <- function(query = "rstats", n_posts = 2000, keep = FALSE) {
 
   top_date_span <- as.integer(difftime(max(posts$date, na.rm = TRUE), 
     min(posts$date, na.rm = TRUE), units = "days") + 1)
+  
+  all_date_span <- as.integer(difftime(max(posts_scored$date, na.rm = TRUE), 
+    min(posts_scored$date, na.rm = TRUE), units = "days") + 1)
+  
+  units_top <- "days"
+  units_all <- "days"
+
+  if (top_date_span == 1) {
+    units_top <- "day"
+  }
+
+  if (all_date_span == 1) {
+    units_all <- "day"
+  }
 
   # print in console
   cat(
     paste0(
-      "\n", posts[["order"]], ". ", posts[["author_displayName"]], " ",
-      posts[["date"]], "\n",
-      "sociality score: ", posts[["score"]], " (", posts[["like_count"]], " likes, ",
-      posts[["repost_count"]], " reposts)", "\n\n",
-      posts[["record_text"]], "\n",
-      posts[["post_url"]], "\n",
+      "\n\n", posts[["order"]], ". ", posts[["author_displayName"]], " ",
+      posts[["date"]],
+      "\n",
+      posts[["score"]], " sociality",
+      "\n",
+      "(", posts[["like_count"]], " likes, ", posts[["repost_count"]],
+      " reposts, ", posts[["reply_count"]], " replies)",
+      "\n\n",
+      posts[["record_text"]],
+      "\n",
+      posts[["post_url"]],
+      "\n",
       "-----", "\n",
       sep = ""
     ),
-    "\n", "These top ", n_keep, " posts occurred over the course of ", top_date_span, " days",
-    "\n", "The total set of ", n_posts, " posts occurred over the course of ", all_date_span, " days", "\n\n",
+    "\n", "These top ", n_keep, " posts occurred over ", top_date_span, " days",
+    "\n", "The total set of ", n_posts, " posts occurred over ", all_date_span, " days", "\n\n",
     sep = ""
   )
 
